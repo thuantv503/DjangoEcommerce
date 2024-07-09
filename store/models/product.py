90b8425c535c0ca8043fd97a00c2a482d1c62b1a
ThuanTruong <thuantv503@gmail.com>
2024-07-09 20:31:23 +0700

    update models and add product model

---
+++ store/models/product.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .category import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField(default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
