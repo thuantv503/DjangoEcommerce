e356848bfd1ec49439e2e17b1802e87dcc94dd23
ThuanTruong <thuantv503@gmail.com>
2024-07-11 21:04:28 +0700

    complete display quantity in cart

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from .views.cart import cart
 from .views.home import store, Index
 from .views.login import logout_request, login_request
 from .views.registration import registration_request
@@ -10,10 +12,11 @@ urlpatterns = [
                   # route is a string contains a URL pattern
                   # view refers to the view function
                   # name the URL
-
-                  path(route='', view=store, name='store'),
+                  path(route='', view=Index.as_view(), name='homepage'),
+                  path(route='index/', view=store, name='store'),
                   path(route='logout/', view=logout_request, name='logout'),
                   path(route='login/', view=login_request, name='login'),
                   path(route='registration/', view=registration_request, name='registration'),
+                  path(route='cart/', view=cart, name='cart')
 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
